cfb1196241292af0863de02e67b327fed57edbcb
Natarajan Lekshmi Narayana Pillai <113646454+natapillai@users.noreply.github.com>
2022-11-07 17:29:30 -0500

    Update twitter_scrape_twint.py

--- twitter_scrape_twint.py
+++ twitter_scrape_twint.py
@@ -1,10 +1,20 @@
 import twint
 
 # Configure
-c = twint.Config()
-c.Profile_full = True
-c.Username = "realDonaldTrump"
-c.Search = "great"
+# c = twint.Config()
+# c.Limit = 1
+# c.Username = "bostonpolice"
+#
+# # Run
+# twint.run.Search(c)
 
+# Configure
+c = twint.Config()
+c.Lang = "en"
+c.Hide_output = True
+c.Username = "bostonpolice"
+c.Search = ['investigation']
+c.Limit = 1
 # Run
 twint.run.Search(c)
+
