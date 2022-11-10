cc8e35527b0a50470c7d629ffb2ca5ae4b740f78
sayeedahmed01 <ahmed.say@northeastern.edu>
2022-11-10 18:43:15 -0500

    Updated Data Scrapper

--- Data_Scrapper/nypd_data_scrapper.py
+++ Data_Scrapper/nypd_data_scrapper.py
@@ -1,15 +1,5 @@
-#!/usr/bin/env python
-
-# make sure to install these packages before running:
-# pip install pandas
-# pip install sodapy
-
 import pandas as pd
 from sodapy import Socrata
-
-# Unauthenticated client only works with public data sets. Note 'None'
-# in place of application token, and no username or password:
-# client = Socrata("data.cityofnewyork.us", None)
 
 # Example authenticated client (needed for non-public datasets):
 MyAppToken = "uy31BMUiTPtOGaAzhZkjoevas"
@@ -20,16 +10,22 @@ client = Socrata("data.cityofnewyork.us",
 
 # First 2000 results, returned as JSON from API / converted to Python list of
 # dictionaries by sodapy.
-results = client.get("qgea-i56i", limit=50000)
+new_york_results = client.get("qgea-i56i", limit=50000)
 
 # Convert to pandas DataFrame
-results_df = pd.DataFrame.from_records(results).drop([':@computed_region_efsh_h5xi',':@computed_region_f5dn_yrer', ':@computed_region_yeji_bk3q',
+new_york_results_df = pd.DataFrame.from_records(new_york_results).drop([':@computed_region_efsh_h5xi',':@computed_region_f5dn_yrer', ':@computed_region_yeji_bk3q',
                                                     ':@computed_region_92fq_4b7q', ':@computed_region_sbqj_enih'], axis=1)
 # print(results_df.head(1))
 print("Columns in the data fetched : \n")
-columns = results_df.columns
+
+columns = new_york_results_df.columns
 print(columns)
+print(new_york_results_df.head(5))
 print(columns.__len__())
+
+boston_url = "https://data.boston.gov/dataset/6220d948-eae2-4e4b-8723-2dc8e67722a3/resource/313e56df-6d77-49d2-9c49-ee411f10cf58/download/tmpobugbywv.csv"
+boston_data = pd.read_csv(boston_url)
+
 
 
 
