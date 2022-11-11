a9f591e4de78a94630879a4a03144b266782463a
Chaman Betrabet <113218344+chamanbetra@users.noreply.github.com>
2022-11-10 19:13:02 -0500

    added twitter scraper

--- Twitter Scrapping/twitter_scraper_json.py
+++ Twitter Scrapping/twitter_scraper_json.py
@@ -1,9 +1,8 @@
-import tweepy
-import json
-import sys
-import configparser
-import pandas as pd
-from sqlalchemy import create_engine
+import tweepy  # importing tweepy library for twitter API
+import configparser  # importing configparser for reading config.ini
+import pandas as pd # importing pandas for dataframe related operations
+from sqlalchemy import create_engine # for sql related operations
+from datetime import datetime # for date conversion
 
 config = configparser.ConfigParser()
 
@@ -18,61 +17,57 @@ access_token_secret = config['twitter']['access_token_secret']
 auth = tweepy.OAuthHandler(api_key, api_key_secret)
 auth.set_access_token(access_token, access_token_secret)
 
-api=tweepy.API(auth)
+api = tweepy.API(auth)
 
 alltweets = []
 
-new_tweets = api.user_timeline(screen_name ='NYPDTips', count=200)
-alltweets.extend(new_tweets)
+keyword_list = ['NYPDTips', 'bostonpolice', 'FairfieldPolice', 'crime', 'gunshot']
+#
+for keyword in keyword_list:
+    new_tweets = tweepy.Cursor(api.search_tweets, q=keyword).items(30)
+    alltweets.extend(new_tweets)
 
 tweet_list = []
+tweet_userlist = []
 
 for tweet in alltweets:
-    tweet_information =dict()
-    tweet_information['id_str']=tweet.id_str
-    tweet_information['text']=tweet.text
-
-    tweet_information['created_at']=tweet.created_at.strftime("%Y-%m-%d %H:%M:%S")
-    tweet_information['retweet_count']=tweet.retweet_count
-    tweet_information['favorite_count']=tweet.favorite_count
+    tweet_information = dict()
+    tweet_userinfo = dict()
     user_dictionary = tweet._json['user']
-    tweet_information['location']=user_dictionary['location']
-    tweet_information['followers_count']=user_dictionary['followers_count']
-    tweet_information['screen_name']=user_dictionary['screen_name']
+    # appending user information
+    tweet_userinfo['id'] = user_dictionary['id']
+    tweet_userinfo['screen_name'] = user_dictionary['screen_name']
+    tweet_userinfo['followers_count'] = user_dictionary['followers_count']
+    datetime_object = datetime.strftime(
+        datetime.strptime(user_dictionary['created_at'], '%a %b %d %H:%M:%S +0000 %Y'), '%Y-%m-%d %H:%M:%S')
+    tweet_userinfo['created_at'] = datetime_object
 
+    # appending tweet information
+    tweet_information['id_str'] = tweet.id_str
+    tweet_information['text'] = tweet.text
+    tweet_information['created_at'] = tweet.created_at.strftime("%Y-%m-%d %H:%M:%S")
+    tweet_information['retweet_count'] = tweet.retweet_count
+    tweet_information['favorite_count'] = tweet.favorite_count
+    place = user_dictionary['location']
+    string_list = place.split(',')
+    try:
+        tweet_information['city'] = string_list[0]
+        tweet_information['state'] = string_list[1]
+    except IndexError:
+        pass
+    tweet_information['country'] = "United States"
+    tweet_information['id'] = user_dictionary['id']
     tweet_list.append(tweet_information)
-# file = 'tweets.json'
-# file_des = open(file,'w')
-# json.dump(tweet_list,file_des,indent=4,sort_keys=True)
-#
-# file_des.close()
+    tweet_userlist.append(tweet_userinfo)
+data_df = pd.DataFrame(tweet_list).drop_duplicates()
+data_df_user = pd.DataFrame(tweet_userlist).drop_duplicates()
 
-# data_df = pd.read_json('tweets.json')
-data_df = pd.DataFrame(tweet_list)
-#print(data_df)
-# print(data_df .head(10))
-# connection = pymysql.connect(host='localhost',
-#                              user='root',
-#                              password='1996Ch1609*')
-
-# cursor = connection.cursor()
-
-# sql = "create database crimeboard"
-# cursor.execute(sql)
-
-
-# connection.commit()
-
+# connecting to SQL DB
 engine = create_engine("mysql+pymysql://{user}:{pw}@localhost/{db}"
                        .format(user="root",
                                pw="1996Ch1609*",
                                db="crimeboard"))
 
-data_df.to_sql('tweets', con = engine, if_exists='append', chunksize=100, index=False)
-
-
-
-
-
-
-
+# converting dataframe to SQL table
+data_df.to_sql('tweets', con=engine, if_exists='append', chunksize=100, index=False)
+data_df_user.to_sql('tweet_users', con=engine, if_exists='append', chunksize=100, index=False)
