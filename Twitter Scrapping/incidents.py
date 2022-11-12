7ef485c072bfe873d3fa90328f9f0c801f4f1d1f
Natarajan Lekshmi Narayana Pillai <113646454+natapillai@users.noreply.github.com>
2022-11-12 13:52:41 -0500

    Update incidents.py

--- Twitter Scrapping/incidents.py
+++ Twitter Scrapping/incidents.py
@@ -13,22 +13,58 @@ geolocator = Nominatim(user_agent="geoapiExercises")
 data = pd.read_csv('boston_2022.csv', low_memory=False, nrows=10).drop_duplicates()
 
 df1 = []
+df2 = []
+df3 = []
+df4 = []
+df5 = []
+df6 = []
+df7 = []
 
 for i in range(0, len(data)):
     lat = data.iloc[i]['Lat']
-    # print(lat)
     long = data.iloc[i]['Long']
     location = geolocator.reverse(lat.astype(str)+","+long.astype(str))
 
     address = location.raw['address']
 
+    suburb = address.get('suburb','')
     city = address.get('city','')
+    county = address.get('county','')
+    state = address.get('state','')
+    postcode = address.get('postcode','')
+    country = address.get('country','')
+    country_code = address.get('country_code','')
 
-    df1.append(city)
+    df1.append(suburb)
+    df2.append(city)
+    df3.append(county)
+    df4.append(state)
+    df5.append(postcode)
+    df6.append(country)
+    df7.append(country_code)
 
-df2 = pd.DataFrame(df1)
-df2.columns=['City']
+dfA1 = pd.DataFrame(df1)
+dfA2 = pd.DataFrame(df2)
+dfA3 = pd.DataFrame(df3)
+dfA4 = pd.DataFrame(df4)
+dfA5 = pd.DataFrame(df5)
+dfA6 = pd.DataFrame(df6)
+dfA7 = pd.DataFrame(df7)
 
-data = pd.concat([data, df2], axis=1)
+dfA1.columns=['Suburb']
+dfA2.columns=['City']
+dfA3.columns=['County']
+dfA4.columns=['State']
+dfA5.columns=['Postcode']
+dfA6.columns=['Country']
+dfA7.columns=['Country_Code']
+
+data = pd.concat([data, dfA1], axis=1)
+data = pd.concat([data, dfA2], axis=1)
+data = pd.concat([data, dfA3], axis=1)
+data = pd.concat([data, dfA4], axis=1)
+data = pd.concat([data, dfA5], axis=1)
+data = pd.concat([data, dfA6], axis=1)
+data = pd.concat([data, dfA7], axis=1)
 
 data.to_sql('incidents', con=engine, if_exists='append', chunksize=100, index=False)
