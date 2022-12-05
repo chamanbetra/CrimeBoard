c087d52d00b594e357f75f45c91ab55052f95c6e
Natarajan Lekshmi Narayana Pillai <113646454+natapillai@users.noreply.github.com>
2022-12-05 13:41:04 -0500

    Add files via upload

--- Data_Scrapper/data_scrapper.py
+++ Data_Scrapper/data_scrapper.py
@@ -26,26 +26,47 @@ print(df_shots_fired.head())
 print(df_shooting.head())
 print(df_firearm_recovery.head())
 
+print(df_shooting.columns)
+print(df_incident.columns)
+
 engine = create_engine("mysql+pymysql://{user}:{pw}@localhost/{db}"
                        .format(user="root",
                                pw="1996Ch1609*",
                                db="crimeboard"))
 df_offence_codes =  df_offence_codes.drop_duplicates(subset='offense_code', keep="first")
-df_offence_codes.to_sql('offense', con=engine, if_exists='append', chunksize=100, index=False)
-df_district_codes.to_sql('districts', con=engine, if_exists='append', chunksize=100, index=False)
+# df_offence_codes.to_sql('offense', con=engine, if_exists='append', chunksize=100, index=False)
+# df_district_codes.to_sql('districts', con=engine, if_exists='append', chunksize=100, index=False)
 
 df_shooting['shooting_date'] = pd.to_datetime(df_shooting['shooting_date'])
+
+cond_shooting = ~df_shooting['incident_number'].isin(df_incident['incident_number'])
+cond_shooting1 = ~df_incident['incident_number'].isin(df_shooting['incident_number'])
+
+df_shooting.drop(df_shooting[cond_shooting].index, inplace=True)
+df_incident.drop(df_incident[cond_shooting1].index, inplace=True)
+
+df_shooting.drop_duplicates(subset=['incident_number'], inplace=True)
+
+
+cond_shotsfired = ~df_shots_fired['incident_number'].isin(df_incident['incident_number'])
+# cond_shotsfired1 = ~df_incident['incident_number'].isin(df_shots_fired['incident_number'])
+# #
+# df_shots_fired.drop(df_shots_fired[cond_shotsfired].index, inplace=True)
+# df_shots_fired.drop_duplicates(subset=['incident_number'], inplace=True)
+print(df_shots_fired)
+# df_incident.drop(df_incident[cond_shotsfired1].index, inplace=True)
+
+
 df_shooting.to_sql('shooting', con=engine, if_exists='append', chunksize=100, index=False)
-
 df_incident.to_sql('incidents', con=engine, if_exists='append', chunksize=100, index=False)
-
-
-
+#
+#
+#
 df_shots_fired['incident_date'] = pd.to_datetime(df_shots_fired['incident_date'])
 df_shots_fired.to_sql('shots_fired', con=engine, if_exists='append', chunksize=100, index=False)
-
-
-
-
-
-df_firearm_recovery.to_sql('firearm_recovery', con=engine, if_exists='append', chunksize=100, index=False)
+#
+#
+#
+#
+#
+# df_firearm_recovery.to_sql('firearm_recovery', con=engine, if_exists='append', chunksize=100, index=False)
